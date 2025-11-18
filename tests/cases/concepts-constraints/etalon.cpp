913c33f305ccfbbbc52261e007a692c74cb45f4e
Karthikeya Kona <konakarthik12@gmail.com>
2025-11-17 23:22:25 -0500

    add advanced concepts test

--- tests/cases/concepts-constraints/etalon.cpp
+++ tests/cases/concepts-constraints/etalon.cpp
@@ -1,3 +1,7 @@
+#include <concepts>
+
+
+// Basic concept usage
 template <typename T>
 concept Add = requires(T a, T b) {
     { a + b };
@@ -10,36 +14,114 @@ T add(T a, T b) {
 }
 
 template <typename T>
-concept Add2 = requires(T a, T b) { { a + b }; };
-
-template <Add2 T>
-T add2(T a) { return a; }
+concept AlwaysTrue = true;
 
 template <typename T>
-concept Add3 = requires(T a, T b) { { a + b }; };
+    requires AlwaysTrue<T>
+void f(T) {}
 
-auto add3(Add3 auto a) { return a; }
+// Concept used ONLY in requires requires clause (key test case)
+template <typename T>
+concept HasIter = requires(T& t) {
+    t.begin();
+    t.end();
+};
+
+// Unused concept - should be removed
+
+
+// Concept used in requires requires with multiple concepts
+template <typename T>
+concept HasSize = requires(T& t) {
+    t.size();
+};
 
 template <typename T>
-constexpr bool AlwaysTrue = true;
+class Container {
+public:
+    // Concept used ONLY in requires requires clause
+    template <std::integral First>
+    requires requires(T& container) {
+        { HasIter<T> };
+    }
+    explicit Container(First first) {
+        (void)first;
+    }
+
+    // Multiple concepts in requires requires
+    template <std::integral First>
+    requires requires(T& container) {
+        { HasSize<T> };
+        { HasIter<T> };
+    }
+    explicit Container(First first, int) {
+        (void)first;
+    }
+
+    // Concept used in requires requires in method
+    template <typename U>
+    requires requires(U& u) {
+        { HasIter<U> };
+    }
+    void process(U& item) {
+        (void)item;
+    }
+
+    // Concept used in both requires requires AND if constexpr
+    template <typename U>
+    requires requires(U& u) {
+        { HasIter<U> };
+    }
+    void processWithIfConstexpr(U& item) {
+        if constexpr (HasIter<U>) {
+            item.begin();
+        }
+    }
+};
+
+// Concept used in requires requires with nested requires
+template <typename T>
+concept NestedConcept = requires(T& t) {
+    requires HasIter<T>;
+    t.something();
+};
 
 template <typename T>
-concept Concept4 = AlwaysTrue<T>;
+class NestedContainer {
+public:
+    template <std::integral First>
+    requires requires(T& container) {
+        { NestedConcept<T> };
+    }
+    explicit NestedContainer(First first) {
+        (void)first;
+    }
+};
 
-namespace test5 {
-    template <typename T>
-    constexpr bool AlwaysTrue = true;
+struct WithIter {
+    int* begin() { return nullptr; }
+    int* end() { return nullptr; }
+    
+};
 
-    template <typename T>
-        requires AlwaysTrue<T>
-    void f(T) {}
-}
 
 int main() {
-    add(-5, 5);
-    add2(1);
-    add3(1);
-    Concept4 auto i = 1;
-    test5::f(1);
+    // Basic concept usage
+    add(1, 2);
+    f(1);
+    
+    // Concept used in requires requires
+    Container<WithIter> c1(5);
+    Container<WithIter> c2(10, 0);
+    
+    // Concept used in method requires requires
+    WithIter w;
+    Container<int> c3(5);
+    c3.process(w);
+    c3.processWithIfConstexpr(w);
+    
+    // Nested concept
+    NestedContainer<WithIter> nc(5);
+    
+    return 0;
 }
-
